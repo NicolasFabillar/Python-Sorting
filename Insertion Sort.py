8c29f43da1378d221731fa760c5dd327691335e0
NicolasFabillar <nicosfabillar09@gmail.com>
2023-01-22 15:16:58 +0800

    Finished the Insertion Sort code. I added a print statement for each iteration to determine if the function works properly.

--- Insertion Sort.py
+++ Insertion Sort.py
@@ -1,3 +1,22 @@
 print(f'{"*** Programmed by ***": ^40}')
 print(f'{"*** Nicolas Fabillar ***": ^40}')
 print()
+
+def insertion_sort (Numbers):
+    for i in range(1, len(Numbers)):
+        anchor = Numbers[i]
+        j = i - 1
+        while j >= 0 and anchor < Numbers[j]:
+            Numbers[j+1] = Numbers[j]
+            j -= 1
+
+        Numbers[j+1] = anchor
+
+        # Prints the number list after the swap.
+        print("After", i, "iteration(s), the sequence is:", Numbers)
+
+Number_List = [71, 88, 76, 82, 52, 48, 10, 16, 30, 86]
+insertion_sort(Number_List)
+print("\nThe result of the Bubble sort is:", Number_List)
+
+
