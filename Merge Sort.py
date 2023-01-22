6e4d49bc5c286115bf7692e52d176d463ebfce5b
NicolasFabillar <nicosfabillar09@gmail.com>
2023-01-22 15:19:56 +0800

    Create a file for merge sort. Trying to code now.

---
+++ Merge Sort.py
@@ -0,0 +1,10 @@
+print(f'{"*** Programmed by ***": ^40}')
+print(f'{"*** Nicolas Fabillar ***": ^40}')
+print()
+
+def merge_sort (Numbers):
+    
+
+Number_List = [71, 88, 76, 82, 52, 48, 10, 16, 30, 86]
+merge_sort(Number_List)
+print("\nThe result of the Bubble sort is:", Number_List)
